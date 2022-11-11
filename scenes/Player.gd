fde856d5cdb1fdb2cb6ab8ec4d3c6719542db30d
N0TKEY <n0tkey@ya.ru>
2022-11-11 14:16:38 +0300

    Правки
    Вернул гравитацию при падении.
    Сделал статичную камеру на уровень.
    Перенёс debug окно в статичную позицию.

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,51 +1,61 @@
 extends KinematicBody2D
 var speed = 120
-const gravitation = 0.3
-var gravity = gravitation
-var jump = 4000
+var jump = 3800
 var jumpLine = 0
+const gravitation = 3.5
 var fly = false
 var fall = false
 const FLOOR = Vector2(0,-1)
 var velocity = Vector2(0,0)
 var duble_jump = false
+onready var newLabel = get_parent().get_node("Label")
 
 func _physics_process(delta):
-	$Label.text = ""
+	newLabel.text = ""
 	if is_on_floor():
-		gravity = 0.5
 		jumpLine = 0
 #		$CircleAnimation.visible = false
 		duble_jump = false
 		fall = false
+	elif !fly:
+		fall = true
 	if InputEventAction.new():
 		velocitySet()
 	
 	if fly:
 		velocity.y = -1
+		if jumpLine >= jump and fly:
+			fly = false
+			jumpLine = 0
+			fall = true
 	elif !is_on_floor():
 		velocity.y = 1
-	if jumpLine >= jump:
-		fly = false
-		fall = true
+	
 	
 	velocity.x *= speed
-	if !$Button.disabled:
-		if is_on_wall() and fall:
-			velocity.y *= speed
+	if is_on_wall() and fall:
+		velocity.y *= speed
+	else:
+		newLabel.text += str(jumpLine>jump*0.9 and fly) + "\n" + str(jumpLine<jump*0.1 and fall) + str(jumpLine) + "\n"
+		if (jumpLine>jump*0.9 and fly) or (jumpLine<jump*0.1 and fall):
+			velocity.y *= speed*1
 		else:
-			velocity.y *= speed*2.5
-	jumpLine -= velocity.y
-	$Label.text += "Velo: " + str(velocity) + "\n"
+			if fall:
+				velocity.y *= speed+int(gravitation*sqrt(jumpLine))
+				print(int(gravitation*sqrt(jumpLine)))
+			else:
+				velocity.y *= speed*2.5
+	jumpLine += abs(velocity.y)
+	newLabel.text += "Velo: " + str(velocity) + "\n"
 	velocity = move_and_slide(velocity, FLOOR)
-	$Label.text += "Floor: " + str(is_on_floor()) + "\n"
+	newLabel.text += "Floor: " + str(is_on_floor()) + "\n"
+	newLabel.text += "Fall: " + str(fall) + "\n"
 	animates_player(velocity)
 
 
 func velocitySet():
 	if Input.is_action_pressed("ui_left") or $Button.pressed:
 		velocity.x = -1
-		#$Button.disabled = true
 		if !$Sprite.flip_h:
 			$Sprite.flip_h = true
 	elif Input.is_action_pressed("ui_right"):
@@ -54,23 +64,23 @@ func velocitySet():
 			$Sprite.flip_h = false
 	else:
 		velocity.x = 0
-	if Input.is_action_just_pressed("ui_up") and (is_on_floor() or !duble_jump):
+	if Input.is_action_just_pressed("ui_up") and (is_on_floor() or !duble_jump or is_on_wall()):
 		fly = true
-		if !is_on_floor():
+		fall = false
+		jumpLine = 0
+		if !is_on_floor() and !is_on_wall():
 			duble_jump = true
-			jumpLine = 0
-	if $Button2.pressed:
-		$Button.disabled = false
+	#print("Жмак")
 
 
 func animates_player(direction: Vector2):
-	$Label.text += "Fly: " + str(fly) + "\n"
+	newLabel.text += "Fly: " + str(fly) + "\n"
 	if duble_jump and fly:
 			$Sprite.play("D_Jump")
 	else:
 		if direction.x != 0:
 			$Sprite.play("Run")
-		elif !is_on_floor() and (fall or fly):
+		elif !is_on_floor() and (fall or fly) and !is_on_wall():
 			if fall:
 				$Sprite.play("Fall")
 			elif fly:
